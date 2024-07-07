4c1290d376c79ec2cc35ed31e856eccd2d4872d3
Sahidh <sahidhh5@gmail.com>
2024-07-07 12:23:57 +0530

    added navbar and patients section

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,71 @@
+import { Avatar, Menu } from "antd";
+import { Header } from "antd/es/layout/layout";
+import "./navbar.css";
+
+export const Navbar = () => {
+  const items = [
+    { key: 1, label: "Overview" },
+    { key: 2, label: "Patients" },
+    { key: 3, label: "Schedule" },
+    { key: 4, label: "Messages" },
+    { key: 5, label: "Transactions" },
+  ];
+
+  return (
+    <Header
+      style={{
+        position: "sticky",
+        margin: "15px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        borderRadius: "50px",
+        backgroundColor: "white",
+        height: "55px",
+        padding: "0 10px",
+      }}
+    >
+      <div className="header-logo">
+        <img src="src\assets\TestLogo.svg" alt="" />
+      </div>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={["2"]}
+        items={items}
+        style={{ minWidth: 0, backgroundColor: "white" }}
+      />
+
+      <div className="header-user">
+        <div className="header-user-current">
+          <div>
+            <Avatar
+              size={"large"}
+              src={
+                "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+              }
+            />
+          </div>
+          <div className="header-user-current-details">
+            <p className="header-user-current-name">Dr.Jose Simmons</p>
+            <p className="header-user-current-title">General Practitioner</p>
+          </div>
+        </div>
+        <div className="header-user-options">
+          <div className="header-user-options-settings">
+            <img
+              src="src\assets\settings_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+          <div className="header-user-options-more">
+            <img
+              src="src\assets\more_vert_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </Header>
+  );
+};
---
+++ src/components/PatientsSection/index.tsx
@@ -0,0 +1,334 @@
+import { Avatar } from "antd";
+import './patientsSection.css'
+
+export const PatientsList = () => {
+  return (
+    <div
+      className="content-patients-container"
+      style={{
+        minHeight: 380,
+        background: "#ffffff",
+        borderRadius: "8px",
+        width: "25%",
+        padding: "5px",
+      }}
+    >
+      <div className="content-patients-header">
+        <h3>Patients</h3>
+        <div>
+          <img src="src\assets\search_FILL0_wght300_GRAD0_opsz24.svg" alt="" />
+        </div>
+      </div>
+      <div className="content-patients-list">
+        <div className="content-patients-list-item">
+          <div className="content-patients-list-item-data">
+            <div>
+              <Avatar
+                size={"large"}
+                src={
+                  "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+                }
+              />
+            </div>
+            <div className="content-patient-details">
+              <p className="content-patient-name">Dr.Jose Simmons</p>
+              <p className="content-patient-basic-info">Male, 30</p>
+            </div>
+          </div>
+          <div className="content-patients-list-item-options">
+            <img
+              src="src\assets\more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="content-patients-list-item">
+          <div className="content-patients-list-item-data">
+            <div>
+              <Avatar
+                size={"large"}
+                src={
+                  "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+                }
+              />
+            </div>
+            <div className="content-patient-details">
+              <p className="content-patient-name">Dr.Jose Simmons</p>
+              <p className="content-patient-basic-info">Male, 30</p>
+            </div>
+          </div>
+          <div className="content-patients-list-item-options">
+            <img
+              src="src\assets\more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="content-patients-list-item">
+          <div className="content-patients-list-item-data">
+            <div>
+              <Avatar
+                size={"large"}
+                src={
+                  "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+                }
+              />
+            </div>
+            <div className="content-patient-details">
+              <p className="content-patient-name">Dr.Jose Simmons</p>
+              <p className="content-patient-basic-info">Male, 30</p>
+            </div>
+          </div>
+          <div className="content-patients-list-item-options">
+            <img
+              src="src\assets\more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="content-patients-list-item">
+          <div className="content-patients-list-item-data">
+            <div>
+              <Avatar
+                size={"large"}
+                src={
+                  "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+                }
+              />
+            </div>
+            <div className="content-patient-details">
+              <p className="content-patient-name">Dr.Jose Simmons</p>
+              <p className="content-patient-basic-info">Male, 30</p>
+            </div>
+          </div>
+          <div className="content-patients-list-item-options">
+            <img
+              src="src\assets\more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="content-patients-list-item">
+          <div className="content-patients-list-item-data">
+            <div>
+              <Avatar
+                size={"large"}
+                src={
+                  "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+                }
+              />
+            </div>
+            <div className="content-patient-details">
+              <p className="content-patient-name">Dr.Jose Simmons</p>
+              <p className="content-patient-basic-info">Male, 30</p>
+            </div>
+          </div>
+          <div className="content-patients-list-item-options">
+            <img
+              src="src\assets\more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="content-patients-list-item">
+          <div className="content-patients-list-item-data">
+            <div>
+              <Avatar
+                size={"large"}
+                src={
+                  "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+                }
+              />
+            </div>
+            <div className="content-patient-details">
+              <p className="content-patient-name">Dr.Jose Simmons</p>
+              <p className="content-patient-basic-info">Male, 30</p>
+            </div>
+          </div>
+          <div className="content-patients-list-item-options">
+            <img
+              src="src\assets\more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="content-patients-list-item">
+          <div className="content-patients-list-item-data">
+            <div>
+              <Avatar
+                size={"large"}
+                src={
+                  "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+                }
+              />
+            </div>
+            <div className="content-patient-details">
+              <p className="content-patient-name">Dr.Jose Simmons</p>
+              <p className="content-patient-basic-info">Male, 30</p>
+            </div>
+          </div>
+          <div className="content-patients-list-item-options">
+            <img
+              src="src\assets\more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="content-patients-list-item">
+          <div className="content-patients-list-item-data">
+            <div>
+              <Avatar
+                size={"large"}
+                src={
+                  "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+                }
+              />
+            </div>
+            <div className="content-patient-details">
+              <p className="content-patient-name">Dr.Jose Simmons</p>
+              <p className="content-patient-basic-info">Male, 30</p>
+            </div>
+          </div>
+          <div className="content-patients-list-item-options">
+            <img
+              src="src\assets\more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="content-patients-list-item">
+          <div className="content-patients-list-item-data">
+            <div>
+              <Avatar
+                size={"large"}
+                src={
+                  "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+                }
+              />
+            </div>
+            <div className="content-patient-details">
+              <p className="content-patient-name">Dr.Jose Simmons</p>
+              <p className="content-patient-basic-info">Male, 30</p>
+            </div>
+          </div>
+          <div className="content-patients-list-item-options">
+            <img
+              src="src\assets\more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="content-patients-list-item">
+          <div className="content-patients-list-item-data">
+            <div>
+              <Avatar
+                size={"large"}
+                src={
+                  "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+                }
+              />
+            </div>
+            <div className="content-patient-details">
+              <p className="content-patient-name">Dr.Jose Simmons</p>
+              <p className="content-patient-basic-info">Male, 30</p>
+            </div>
+          </div>
+          <div className="content-patients-list-item-options">
+            <img
+              src="src\assets\more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="content-patients-list-item">
+          <div className="content-patients-list-item-data">
+            <div>
+              <Avatar
+                size={"large"}
+                src={
+                  "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+                }
+              />
+            </div>
+            <div className="content-patient-details">
+              <p className="content-patient-name">Dr.Jose Simmons</p>
+              <p className="content-patient-basic-info">Male, 30</p>
+            </div>
+          </div>
+          <div className="content-patients-list-item-options">
+            <img
+              src="src\assets\more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="content-patients-list-item">
+          <div className="content-patients-list-item-data">
+            <div>
+              <Avatar
+                size={"large"}
+                src={
+                  "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+                }
+              />
+            </div>
+            <div className="content-patient-details">
+              <p className="content-patient-name">Dr.Jose Simmons</p>
+              <p className="content-patient-basic-info">Male, 30</p>
+            </div>
+          </div>
+          <div className="content-patients-list-item-options">
+            <img
+              src="src\assets\more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="content-patients-list-item">
+          <div className="content-patients-list-item-data">
+            <div>
+              <Avatar
+                size={"large"}
+                src={
+                  "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+                }
+              />
+            </div>
+            <div className="content-patient-details">
+              <p className="content-patient-name">Dr.Jose Simmons</p>
+              <p className="content-patient-basic-info">Male, 30</p>
+            </div>
+          </div>
+          <div className="content-patients-list-item-options">
+            <img
+              src="src\assets\more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="content-patients-list-item">
+          <div className="content-patients-list-item-data">
+            <div>
+              <Avatar
+                size={"large"}
+                src={
+                  "src/assets/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+                }
+              />
+            </div>
+            <div className="content-patient-details">
+              <p className="content-patient-name">Dr.Jose Simmons</p>
+              <p className="content-patient-basic-info">Male, 30</p>
+            </div>
+          </div>
+          <div className="content-patients-list-item-options">
+            <img
+              src="src\assets\more_horiz_FILL0_wght300_GRAD0_opsz24.svg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
